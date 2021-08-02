f0c9a76e9d11f961bf36fe913e9dd4562d08e391
arifimran5 <arifimran505@gmail.com>
2021-08-02 18:13:24 +0530

    styled blog page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,39 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import styles from './Blog.module.scss';
+
 const Blog = ({ article }) => {
+  const date = new Date(article.createdAt);
+  const dateObj = {
+    day: date.getDate(),
+    month: date.getMonth(),
+    year: date.getFullYear(),
+  };
   return (
-    <div>
-      <Link href={'/'}>Go back Home</Link>
-      <h1 className="text-4xl font-bold">{article.title}</h1>
-      <ReactMarkdown>{article.body}</ReactMarkdown>
+    <div className="pt-2 mb-10 px-3 sm:px-6 md:pr-10 md:px-20 lg:pl-32 lg:max-w-5xl xl:pl-44 xl:max-w-6xl">
+      <Link href={'/'}>
+        <a className="font-semibold text-gray-400 hover:text-blue-500">{'👈'}Go back Home</a>
+      </Link>
+      <h1 className="text-4xl font-bold mt-8 mb-3">{article.title}</h1>
+      <div className="mb-3">
+        <p className="font-semibold bog_thumb_date inline">{`${dateObj.day}-${dateObj.month}-${dateObj.year}`}</p>
+        <p className="font-semibold text-lg text-gray-500  ml-4 inline">
+          #{article.category.category}
+        </p>
+      </div>
+
+      <Image
+        width="500px"
+        height="240px"
+        src={article.cover.formats.small.url}
+        alt=""
+      />
+      <ReactMarkdown className={styles.article_body}>
+        {article.body}
+      </ReactMarkdown>
     </div>
   );
 };
